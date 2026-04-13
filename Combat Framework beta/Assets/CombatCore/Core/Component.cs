445a7ea0ffe089a11307d2ac3827efd8002c71d2
RedYUkno <90758538+hf1037277956@users.noreply.github.com>
2026-04-13 16:56:52 +0800

    Component逻辑优化

--- Combat Framework beta/Assets/CombatCore/Core/Component.cs
+++ Combat Framework beta/Assets/CombatCore/Core/Component.cs
@@ -1,26 +1,21 @@
-﻿using System.Collections.Generic;
-using CombatCore.Core.Object;
+﻿using CombatCore.Core.Object;
 
 namespace CombatCore.Core
 {
     /// <summary>
-    /// 组件：代表某个实体的扩展功能，而不是某种规则
-    /// 例如一个技能它有自己的伤害数值类型，但“移动”作为一个功能
-    /// 有些技能可能不需要“移动”那么移动则可以抽象为一个组件为需要的技能添加AddComponent
+    /// 组件：代表某个实体的扩展功能或数据承载。
+    /// 遵循极简原则：不持有多余引用，不默认参与 Update 循环。
     /// </summary>
     public class Component : DisposeObject
     {
         /// <summary>
-        /// 组件的父实体
+        /// 组件的父实体 (挂载该组件的容器)
         /// </summary>
         public Entity Parent { get; set; }
-
-        public override bool IsDisposed => base.IsDisposed;
+        
+        public override bool IsDisposed { get; set; }
 
         public bool IsFromPool { get; set; }
-        
-        // 实体的子实体列表
-        public Dictionary<long, Entity> Id2Children { get; private set; } = new();
         
         private bool _enable = false;
         
@@ -33,86 +28,49 @@ namespace CombatCore.Core
                 if (_enable) OnEnable();
                 else OnDisable();
             }
-            get
-            {
-                return _enable;
-            }
+            get => _enable;
         }
         
         public bool Disable => _enable == false;
 
-        public T GetParent<T>() where T : CombatCore.Core.Entity
+        public T GetParent<T>() where T : Entity
         {
             return Parent as T;
         }
 
-        public virtual void Awake()
-        {
-
-        }
+        // ==========================================
+        // 生命周期方法 (Lifecycle Methods)
+        // ==========================================
         
-        public virtual void Awake<T>(T a) 
-        {
-            
-        }
+        public virtual void Awake() { }
+        
+        public virtual void Awake<T>(T a) { }
 
-        public virtual void OnEnable()
-        {
+        public virtual void OnEnable() { }
 
-        }
+        public virtual void OnDisable() { }
 
-        public virtual void OnDisable()
-        {
+        public virtual void OnDestroy() { }
 
-        }
-
-        public virtual void Update(float deltaTime)
-        {
-        }
-
-        public virtual void OnDestroy()
-        {
-            
-        }
-
+        // ==========================================
+        // 内存回收逻辑
+        // ==========================================
+        
         public override void Dispose()
         {
+            if (IsDisposed) return;
+            
             Enable = false;
+            OnDestroy();
+            
+            Parent = null;
+            
             IsDisposed = true;
-
+            
             if (IsFromPool)
             {
                 ComponentObjectPool.Instance.Recycle(this);
             }
         }
-
-        // public static void Destroy(Component entity)
-        // {
-        //     try
-        //     {
-        //         entity.OnDestroy();
-        //     }
-        //     catch (Exception e)
-        //     {
-        //         //Log.Error(e);
-        //     }
-        //     entity.Dispose();
-        // }
-
-        // public T Publish<T>(T TEvent) where T : class
-        // {
-        //     Parent.Publish(TEvent);
-        //     return TEvent;
-        // }
-        //
-        // public void Subscribe<T>(Action<T> action) where T : class
-        // {
-        //     Parent.Subscribe(action);
-        // }
-        //
-        // public void UnSubscribe<T>(Action<T> action) where T : class
-        // {
-        //     Parent.UnSubscribe(action);
-        // }
     }
 }
